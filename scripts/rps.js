773a0dd45ad2ddd24cc447b0b6d04caca7620faf
nvhiii <nahikhan16@gmail.com>
2024-02-25 09:33:56 -0500

    updated rps logic, onto playGame()

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,57 +1,73 @@
+// revisited concise code
 const button = document.querySelector("button");
 
-// fxn declarations, will try to implement anon and arrow fxns later
-
+// fxn declaration
 function getComputerChoice() {
-    
-    let choiceNum = Math.floor(Math.random() * 3); // multiply by 3 bc only 3 choices in this game
-    let choices = ["Rock", "Paper", "Scissors"]; // possible choices of computer
-    let selection = choices[choiceNum];
 
-    console.log(`The computer chose ${selection}`)
-    return selection;
+    let choices = ["Rock", "Paper", "Scissors"];
+    let choiceNum = Math.floor(Math.random() * 3);
+    let computerChoice = choices[choiceNum];
+    return computerChoice;
 
 }
 
-// add arrow fxn
+function singleRound() {
 
-const roundOfGame = (playerSelection, computerSelection) => {
+    let playerSelection = prompt("Please enter rock, paper, or scissors."); // enter selection before seeing computer choice
+    let computerSelection = getComputerChoice(); // computer choice stored here
 
-    let me = prompt("Please enter a valid selection in rock, paper, scissors.");
-    let them = getComputerChoice();
-    let firstChar = me.charAt(0);
-    let restOfString = me.slice(1);
-
-    const winMap = {
+    let winMap = { // object key-value pairing for wins
 
         "Rock" : "Scissors",
         "Scissors" : "Paper",
         "Paper" : "Rock"
-
     }
 
-    let formattedSelection = `${firstChar.toUpperCase()}${restOfString.toLowerCase()}`;
+    // formatting so that user input follows map
+    let firstChar = playerSelection.charAt(0);
+    let restOfString = playerSelection.slice(1); // returns remaining string from index 1 to end
 
-    if (formattedSelection === them) {
+    let formattedPlayerSelection = `${firstChar.toUpperCase()}${restOfString.toLowerCase()}`;
 
-        console.log("It's a tie");
-        return "tie";
+    // check if user selection is valid
+    if (winMap.hasOwnProperty(formattedPlayerSelection)) {
+
+        // tied logic
+        if (formattedPlayerSelection === computerSelection) {
+
+            return "tie";
+
+        } else if (winMap[formattedPlayerSelection] === computerSelection) { // player win
+
+            return "win";
+
+        } else { // computer win
+
+            return "loss";
+
+        }
+
+    } else {
+
+        console.log("Please enter a valid value for rock, paper, and scissors!");
 
     }
-
-    if (winMap[formattedSelection] === them) {
-
-        console.log("You win!");
-        return "win";
-
-    }
-
-    console.log("You lose!");
-    return "lose";
 
 }
 
-// implement a playGame fxn
+function playGame() {
 
-button.addEventListener("click", roundOfGame);
+    for (let i = 1; i < 5; i++) {
 
+        singleRound();
+
+    }
+
+}
+
+// test
+// let playa;
+// let computa;
+// console.log(singleRound(playa, computa));
+
+// button.addEventListener("click", singleRound);
