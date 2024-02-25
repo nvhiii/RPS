c309486f8c9d0c882dd0af38cfbe705a87925b79
nvhiii <nahikhan16@gmail.com>
2024-02-24 21:16:04 -0500

    updated using arrow fxn for rps

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,22 +1,54 @@
 const button = document.querySelector("button");
 
+// fxn declarations, will try to implement anon and arrow fxns later
+
 function getComputerChoice() {
     
     let choiceNum = Math.floor(Math.random() * 3); // multiply by 3 bc only 3 choices in this game
-    if (choiceNum === 0) {
+    let choices = ["Rock", "Paper", "Scissors"]; // possible choices of computer
+    let selection = choices[choiceNum];
 
-        console.log("Computer chose Rock!");
-
-    } else if (choiceNum === 1) {
-
-        console.log("Computer chose Papers!");
-
-    } else {
-
-        console.log("Computer chose Scissors!");
-
-    }
+    console.log(`The computer chose ${selection}`)
+    return selection;
 
 }
 
-button.addEventListener("click", getComputerChoice);
+// add arrow fxn
+
+const roundOfGame = (playerSelection, computerSelection) => {
+
+    let me = prompt("Please enter a valid selection in rock, paper, scissors.");
+    let them = getComputerChoice();
+    let firstChar = me.charAt(0);
+    let restOfString = me.slice(1);
+
+    const winMap = {
+
+        "Rock" : "Scissors",
+        "Scissors" : "Paper",
+        "Paper" : "Rock"
+
+    }
+
+    let formattedSelection = `${firstChar.toUpperCase()}${restOfString.toLowerCase()}`;
+
+    if (formattedSelection === them) {
+
+        console.log("It's a tie");
+        return "tie";
+
+    }
+
+    if (winMap[formattedSelection] === them) {
+
+        console.log("You win!");
+        return "win";
+
+    }
+
+    console.log("You lose!");
+    return "lose";
+
+}
+
+button.addEventListener("click", roundOfGame);
